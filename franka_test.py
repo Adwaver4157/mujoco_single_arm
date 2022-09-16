9a802429de143392eb4b1db238a44b0a586be7e2
adwaver <adwaver4157@gmail.com>
2022-09-16 10:12:14 +0900

    fix: change contype and conaffinity of table in table.xml

--- franka_test.py
+++ franka_test.py
@@ -3,6 +3,7 @@ import mujoco_viewer
 import numpy as np
 
 model = mujoco.MjModel.from_xml_path("franka_emika_panda/scene.xml")
+# model = mujoco.MjModel.from_xml_path("franka_emika_panda/test.xml")
 data = mujoco.MjData(model)
 # create the viewer object
 viewer = mujoco_viewer.MujocoViewer(model, data)
@@ -13,7 +14,7 @@ for _ in range(10000):
     if viewer.is_alive:
         mujoco.mj_step(model, data)
         viewer.render()
-        data.ctrl = [0, 2, 0, -1.57079, 0, 1.57079, -0.7853, 255]
+        data.ctrl = [0, 0, 0, -1.57079, 0, 1.57079, 0.7853, 255]
         # data.ctrl = [0, 0, 0, -1.57079, 0, 1.57079, -0.7853, 255]
         """ qpos = [0.0]*9
         qpos[5] = np.pi / 2
@@ -21,7 +22,7 @@ for _ in range(10000):
         qpos[7] = 0
         qpos[8] = 0.04
         data.qpos = qpos """
-        print(data.qpos[4], data.qpos[8])
+        # print(data.qpos[4], data.qpos[8])
         """ print(pre - data.qpos*100000)
         pre = data.qpos * 100000 """
     else:
